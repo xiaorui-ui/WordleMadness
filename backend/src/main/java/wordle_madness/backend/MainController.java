93202989ed41952514ea63b1ed2b1c27d59dfad4
kjw142857 <e0958424@u.nus.edu>
2023-06-23 01:32:36 +0800

    CORS implementation

--- backend/src/main/java/wordle_madness/backend/MainController.java
+++ backend/src/main/java/wordle_madness/backend/MainController.java
@@ -2,11 +2,7 @@ package wordle_madness.backend;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(path="/backend")
@@ -14,6 +10,7 @@ public class MainController {
     @Autowired
     private UserRepository userRepository;
 
+    @CrossOrigin(origins="https://vercel.com/kjw142857/wordle-madness")
     @PostMapping(path="/add")
     public @ResponseBody String addNewUser (@RequestParam String name
             , @RequestParam String password) {
