6adbf82e36e625605f9b734134981bb10e217d19
kjw142857 <e0958424@u.nus.edu>
2023-06-26 01:25:36 +0800

    Resolve data transmitting

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -22,7 +22,7 @@ export default function Login({ setAns, setAllowed, setUser }) {
         axios.get(BACKEND_GET_WORD_LIST, { params: { username: username } })
                 .then((response) => {
                     console.log(response.data);
-                    setAns(JSON.parse(response.data));
+                    setAns(response.data);
                 })
                 .catch((error) => {
                     console.log(error);
@@ -33,7 +33,7 @@ export default function Login({ setAns, setAllowed, setUser }) {
         axios.get(BACKEND_GET_ALLOWED_WORD_LIST, { params: { username: username } })
                 .then((response) => {
                     console.log(response.data);
-                    setAllowed(JSON.parse(response.data));
+                    setAllowed(response.data);
                 })
                 .catch((error) => {
                     console.log(error);
