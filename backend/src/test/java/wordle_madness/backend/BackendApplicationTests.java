74b88de72f4e6563711aa5b1bcc56d0c7b79b8ec
kjw142857 <e0958424@u.nus.edu>
2023-06-28 00:37:09 +0800

    Switch Gradle structure to Maven

--- backend/src/test/java/wordle_madness/backend/BackendApplicationTests.java
+++ backend/src/test/java/wordle_madness/backend/BackendApplicationTests.java
@@ -3,7 +3,7 @@ package wordle_madness.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = BackendApplicationTests.class)
+@SpringBootTest
 class BackendApplicationTests {
 
 	@Test
