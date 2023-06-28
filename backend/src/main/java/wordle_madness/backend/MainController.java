691a9d06ccbfab9632703a3337df9b78e0d2b684
Xiaorui <xiaoruizhang9876@gmail.com>
2023-06-28 17:18:56 +0800

    change deployment link

--- backend/src/main/java/wordle_madness/backend/MainController.java
+++ backend/src/main/java/wordle_madness/backend/MainController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 
 @Controller
-@CrossOrigin(origins = "https://localhost:3000/")
-// @CrossOrigin(origins="https://wordle-madness.vercel.app/")
+// @CrossOrigin(origins = "https://localhost:3000/")
+@CrossOrigin(origins = "https://wordle-madness.vercel.app/")
 @RequestMapping(path = "/backend")
 public class MainController {
     @Autowired
